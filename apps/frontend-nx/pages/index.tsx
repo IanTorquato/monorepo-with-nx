190624b123b783653c4a38d7919c98f0026c4808
IanTorquato <ian@silbeck.com.br>
2021-07-06 14:35:26 -0300

    Consumindo backend com lib do axios

--- apps/frontend-nx/pages/index.tsx
+++ apps/frontend-nx/pages/index.tsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+
+import { api } from '@monorepo-nx-silbeck-clear/axios-config';
 
 export function Index() {
-  const [returnedBack, setReturnedBack] = useState({message: 'Temporário'});
+  const [returnedBack, setReturnedBack] = useState({}as any);
 
-  // useEffect(()=>{
-    // api
-  // },[]);
+  useEffect(()=>{
+    api.get('/').then((response=>setReturnedBack(response))).catch(console.error);
+  }, []);
 
   return (
     <div>
       <br />
 
       <div style={{ textAlign:'center', marginTop: '40px'}}>
-        <b style={{fontSize: '24px'}}> {returnedBack.message} </b>
+        <b style={{fontSize: '24px'}}> {returnedBack.data?.message} </b>
       </div>
     </div>
   );
